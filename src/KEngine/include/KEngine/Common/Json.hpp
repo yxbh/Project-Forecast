4fbd4408c6709bca7cdf8b44095bbdef09ca33d4
Benjamin Huang <y.x.ben.h+git@gmail.com>
2017-02-11 22:01:52 +1100

    WIP KEngine

--- src/KEngine/include/KEngine/Common/Json.hpp
+++ src/KEngine/include/KEngine/Common/Json.hpp
@@ -8,6 +8,6 @@ namespace ke
     // json library from https://github.com/nlohmann/json
     using namespace nlohmann; // alias the nlohmann namespace to make it our own.
 
-    class Json : public nlohmann::json {};
+    using Json = nlohmann::json;
 
 }
