97315fa9467ec7cc19ecd9885ccd7cdbd1cefdf8
Benjamin Huang <y.x.ben.h+github@gmail.com>
2019-03-04 15:21:42 +1100

    turn on log debug print

--- src/ProjectForecast/main.cpp
+++ src/ProjectForecast/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char ** argv)
 {
     std::cout << u8"hello world" << std::endl;
 
+    ke::Log::instance()->set_level(ke::log::level::debug);
     pf::ProjectForecastApp app(argc, argv);
     const int result = app.exec();
     ke::Log::instance()->info("Exit code: {}", result);
